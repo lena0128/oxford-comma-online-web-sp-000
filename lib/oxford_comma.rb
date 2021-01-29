ece06c47c70ddea9268a0ece74aeee7608845309
Lena Hou <lenahou2013@gmail.com>
2021-01-29 19:47:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size < 3
+    return array.join(" and ")
+  else array.size >= 3
+    array[-1] = "and " + array[-1]
+    return array.join(", ")
+  end
 end
+
+#simply the code
+#def oxford_comma(array)
+#return array.join(" and ") if array.size < 3
+  #array[-1] = "and " + array[-1]
+  #array.join(", ")
+#end
